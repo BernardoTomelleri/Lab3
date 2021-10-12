1bb9851ba80638509738dc3a7c483b01beff37fa
Bernardo Tomelleri <bernardo.tomelleri@gmail.com>
2021-10-13 01:55:18 +0200

    Better data, corner frequency fit

--- Es02/bode.py
+++ Es02/bode.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Oct 11 23:05:45 2021
+Created on Wed Oct 13 01:01:39 2021
 
 @author: berni
 """
@@ -12,35 +12,31 @@ import phylab as lab
 tix = False  # manually choose spacing between axis ticks
 tex = False  # LaTeX typesetting maths and descriptions
 
-# Modelli di trasferimento e lineare
-def lin(x, m=1, q=0):
-    return m*x + q
-
+# Modelli di trasferimento
 def lpf(f, fT):
     return 1./np.sqrt(1+(f/fT)**2) 
 
 def hpf(f, fT):
     return 1./np.sqrt(1+(fT/f)**2) 
 
-def bandpass(f, fL, fH):
-    return 
-f, CH1, CH2, phi = np.genfromtxt('./data/rc1k10nF.csv', float, delimiter=',',
+f, CH1, CH2, phi = np.genfromtxt('./data/rc1k10nF_new.csv', float, delimiter=',',
                             skip_header=21, unpack=True)
 
-f_min = 0; f_max = 1e9
+# Input data manipulation
+f_min = 100; f_max = 1e6
 x, y, dx, dy = lab.mesrange(f, CH2, x_min=f_min, x_max=f_max)
-
-dx = np.full(x.shape, 1e-3); dy = 1e-3*np.abs(y) +1e-3
+dx = 0.3/(np.log(10)*x); dy = 1e-3*np.abs(y) +1e-3
+x = np.log10(x)
 
 # Preliminary plot to visualize the sub-interval of data to analyze
-#lab.rc.typeset(usetex=tex, fontsize=12)
+lab.rc.typeset(usetex=tex, fontsize=12)
 fig, ax = plt.subplots()
 grid(ax, xlab='Frequency $f$ [Hz]', ylab='Gain $A(f)$')
 ax.errorbar(x, y, dy, dx, 'k.', ls='-', ms=2, alpha=0.8)
 
-# linear fit
-init = [1, 1]
-model = lin
+# gain fit
+init = [3.8]
+model = lpf
 #par_bounds = [[0.0, 1e-6], [0., 0.1], [-1, 1]]
 #genetic_pars = lab.gen_init(model, coords=[x, y], bounds=par_bounds, unc=dy)
 
@@ -53,7 +49,6 @@ chisq, ndof, resn = chitest(model(x, *pars), y, unc=deff, ddof=len(pars), v=True
 fig, (axf, axr) = pltfitres(model, x, y, dx, deff, pars=pars)
 axf.set_ylabel('Gain $A(f)$')
 if tix: lab.tick(axf, xmaj=5, ymaj=50)
-legend = axf.legend(loc='best')
 
 axr.set_xlabel('Frequency $f$ [Hz]')
 if tix: lab.tick(axr, xmaj=5, ymaj=2, ymin=0.5)
---
+++ Es02/corner.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 11 23:05:45 2021
+
+@author: berni
+"""
+
+from lab import (np, plt, grid, tick, mesrange, propfit, curve_fit, errcor, prnpar, chitest, pltfitres)
+import rc
+
+''' Variables that control the script '''
+tix = False  # manually choose spacing between axis ticks
+tex = True  # LaTeX typesetting maths and descriptions
+
+# Modelli di trasferimento e lineare
+def lin(x, m=1, q=0):
+    return m*x + q
+
+
+f, CH1, CH2, phi = np.genfromtxt('./data/rc1k10nF_new.csv', float, delimiter=',',
+                            skip_header=21, unpack=True)
+
+f_lo = 500; f_hi = 4e4
+
+# Linear fit to high frequency values
+x, y, dx, dy = mesrange(f, CH2, x_min=f_hi)
+dx = 0.3/(np.log(10)*x); dy = 1e-3*np.abs(y) +1e-3
+x = np.log10(x)
+
+# Preliminary plot to visualize the sub-interval of data to analyze
+rc.typeset(usetex=tex, fontsize=12)
+fig, ax = plt.subplots()
+grid(ax, xlab='Frequency $f$ [Hz]', ylab='Gain $A(f)$')
+ax.errorbar(x, y, dy, dx, 'k.', ls='-', ms=2, alpha=0.8)
+
+# linear fit
+init = [-20, 1]
+model = lin
+#par_bounds = [[0.0, 1e-6], [0., 0.1], [-1, 1]]
+#genetic_pars = lab.gen_init(model, coords=[x, y], bounds=par_bounds, unc=dy)
+
+pars, covm, deff = propfit(model, x, y, dx, dy, p0=init, alg='lm')
+perr, pcor = errcor(covm)
+prnpar(pars, perr, model)
+chisq, ndof, resn = chitest(model(x, *pars), y, unc=deff, ddof=len(pars), v=True)
+
+# linear fit graphs
+space = np.linspace(3.5, 6.2, 5000)
+fig, (axf, axr) = pltfitres(model, x, y, dx, deff, pars=pars, space=space)
+axf.set_ylabel('Gain $A(f)$')
+if tix: tick(axf, xmaj=5, ymaj=50)
+
+axr.set_xlabel('Frequency log10($f$) [Hz]')
+if tix: tick(axr, xmaj=5, ymaj=2, ymin=0.5)
+
+# Constant fit for low frequency data
+def const(x, c=0):
+    return c
+
+xl, yl, dxl, dyl = mesrange(f, CH2, x_min=0, x_max=f_lo)
+dxl = 0.3/(np.log(10)*xl); dyl = 1e-2*np.abs(yl) +1e-2
+xl = np.log10(xl)
+
+popt, pcov = curve_fit(const, xl, yl, sigma=dyl)
+errs, corm = errcor(pcov)
+prnpar(popt, errs, const)
+chisq, ndof, resn = chitest(const(xl, *popt), yl, unc=dyl, ddof=len(popt), v=True)
+
+space = np.linspace(1.9, 4, 5000)
+fit = np.full(space.shape, popt[0])
+axf.errorbar(xl, yl, dyl, dxl, 'k.', ls='', ms=2, alpha=0.8)
+axr.errorbar(xl, resn, None, None, **rc.NORMRES_MARKER)
+axf.plot(space, fit, label=r'costant fit $\chi^2 = %.f/%d$' % (chisq, ndof))
+axf.errorbar(np.log10(f), CH2, 1e-3*np.abs(CH2) +1e-3, 0.3/(np.log(10)*f), 'g.', ls='',
+             ms=2, alpha=0.05, zorder=0)
+legend = axf.legend(loc='best')
+
+fc = space[np.argmin(np.abs(popt[0] - lin(space, *pars)))]
+print('Corner frequency:', 10**fc)
