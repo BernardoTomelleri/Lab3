21baba1143f88afaef2ac29efd7ae02963d8e5a0
Bernardo Tomelleri <bernardo.tomelleri@gmail.com>
2021-11-10 03:21:52 +0100

    Es04: Misure & screen invamp, ALPF

--- Es04/gain.py
+++ Es04/gain.py
@@ -16,7 +16,7 @@ tex = True  # LaTeX typesetting maths and descriptions
 def lin(x, m=1, q=0):
     return m*x + q
 
-vin, dvin, vout, dvout = np.loadtxt('./data/Av.txt', float, unpack=True)
+vin, dvin, vout, dvout = np.loadtxt('./data/Av5k1.txt', float, unpack=True)
 v_min = 0; v_max = 3
 
 # Linear fit
@@ -29,10 +29,10 @@ grid(ax, xlab=r'Input amplitude $v_{\rm in}$ [V]', ylab=r'Output amplitude $v_{\
 ax.errorbar(x, y, dy, dx, 'k.', ls='-', ms=2, alpha=0.8)
 
 # linear fit
-init = [7, 0]
+init = [5, 0]
 model = lin
 
-pars, covm, deff = propfit(model, x, y, dx, dy, p0=init, alg='lm', max_iter=3)
+pars, covm, deff = propfit(model, x, y, dx, dy, p0=init, alg='lm', max_iter=1, tail=2)
 perr, pcor = errcor(covm)
 prnpar(pars, perr, model)
 chisq, ndof, resn = chitest(model(x, *pars), y, unc=deff, ddof=len(pars), v=True)
@@ -40,8 +40,8 @@ chisq, ndof, resn = chitest(model(x, *pars), y, unc=deff, ddof=len(pars), v=True
 # linear fit graphs
 fig, (axf, axr) = pltfitres(model, x, y, dx, deff, pars=pars)
 axf.set_ylabel(r'Output amplitude $v_{\rm out}$ [V]')
-if tix: tick(axf, xmaj=5, ymaj=1)
+if tix: tick(axf, xmaj=5, ymaj=0.5)
 
 axr.set_xlabel(r'Input amplitude $v_{\rm in}$ [V]', x=0.8)
-if tix: tick(axr, xmaj=0.1, ymaj=1, ymin=0.2)
+if tix: tick(axr, xmaj=0.1, ymaj=0.1, ymin=0.02)
 legend = axf.legend(loc='best')
