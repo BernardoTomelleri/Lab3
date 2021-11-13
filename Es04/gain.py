85a795e7c4f6e361b6bcd3c6175d0d3d02a6fffc
Bernardo Tomelleri <bernardo.tomelleri@gmail.com>
2021-11-13 17:17:22 +0100

    Es04: saturazione invertente; pinna di squalo

--- Es04/gain.py
+++ Es04/gain.py
@@ -13,14 +13,16 @@ tix = True  # manually choose spacing between axis ticks
 tex = True  # LaTeX typesetting maths and descriptions
 
 # Modello lineare
-def lin(x, m=1, q=0):
-    return m*x + q
+def lin(x, m=1):
+    return m*x
 
 vin, dvin, vout, dvout = np.loadtxt('./data/Av5k1.txt', float, unpack=True)
-v_min = 0; v_max = 3
+vin*=1e-3; dvin*=1e-3; vout*=1e-3; dvout*=1e-3
+v_min = 0; v_max = 0.6
 
 # Linear fit
-x, y, dx, dy = mesrange(1e-3*vin, 1e-3*vout, dx=1e-3*dvin, dy=1e-3*dvout, x_min=v_min)
+x, y, dx, dy = mesrange(vin, vout, dx=dvin, dy=dvout,
+                        x_min=v_min, x_max=v_max)
 
 # Preliminary plot to visualize the sub-interval of data to analyze
 lab.rc.typeset(usetex=tex, fontsize=12)
@@ -29,7 +31,7 @@ grid(ax, xlab=r'Input amplitude $v_{\rm in}$ [V]', ylab=r'Output amplitude $v_{\
 ax.errorbar(x, y, dy, dx, 'k.', ls='-', ms=2, alpha=0.8)
 
 # linear fit
-init = [5, 0]
+init = [5]
 model = lin
 
 pars, covm, deff = propfit(model, x, y, dx, dy, p0=init, alg='lm', max_iter=1, tail=2)
@@ -43,5 +45,14 @@ axf.set_ylabel(r'Output amplitude $v_{\rm out}$ [V]')
 if tix: tick(axf, xmaj=5, ymaj=0.5)
 
 axr.set_xlabel(r'Input amplitude $v_{\rm in}$ [V]', x=0.8)
-if tix: tick(axr, xmaj=0.1, ymaj=0.1, ymin=0.02)
+if tix: tick(axr, xmaj=0.05, ymaj=0.1, ymin=0.02)
 legend = axf.legend(loc='best')
+
+# complete graph with data at opamp saturation
+fig, ax = plt.subplots()
+grid(ax, xlab=r'Input amplitude $v_{\rm in}$ [V]', ylab=r'Output amplitude $v_{\rm out}$ [V]')
+ax.errorbar(vin, vout, dvout, dvin, **lab.rc.MEASURE_MARKER)
+ax.plot(x, model(x, *pars), label='Measured linear gain')
+ax.axhline(3.88, c='r', alpha=0.5, label='Amplifier output saturation $= 3.88 \pm 0.03$ V')
+ax.axhline(3.88 + 28e-3, c='r', ls='--', alpha=0.3); ax.axhline(3.88 - 28e-3, c='r', ls='--', alpha=0.3)
+legend = ax.legend(loc='best')
