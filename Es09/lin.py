b7251ae59aae1edad6646812d9b4533d268079aa
Bernardo Tomelleri <bernardo.tomelleri@gmail.com>
2022-03-30 00:12:51 +0200

    Es e-m: consegna; EsD1: upload

--- Es09/lin.py
+++ Es09/lin.py
@@ -10,7 +10,7 @@ import phylab as lab
 
 ''' Variables that control the script '''
 tix = False  # manually choose spacing between axis ticks
-tex = False  # LaTeX typesetting maths and descriptions
+tex = True  # LaTeX typesetting maths and descriptions
 TEMP = False
 
 # Modello lineare
@@ -40,7 +40,7 @@ ax.errorbar(x, y, dy, dx, 'k.', ls='-', ms=2, alpha=0.8)
 # linear fit
 init = [3, 0]
 model = lin
-
+x, dx, y, dy = y, dy, x, dx
 deff = dy
 pars, covm = lab.curve_fit(model, x, y, sigma=dy, p0=init, absolute_sigma=False)
 perr, pcor = errcor(covm)
@@ -49,7 +49,7 @@ chisq, ndof, resn = chitest(model(x, *pars), y, unc=deff, ddof=len(pars), v=True
 
 # linear fit graphs
 fig, (axf, axr) = pltfitres(model, x, y, dx, deff, pars=pars)
-axf.set_ylabel(r'distance in air $s(t)$ [mm]')
+axf.set_ylabel(r'$(BR)^2$ [T$^2$ m$^2$]')
 if tix: tick(axf, xmaj=0.1, ymaj=50)
 if TEMP:
     axt = axf.twinx()
@@ -59,6 +59,6 @@ if TEMP:
     axt.axhline(2.69 + 1e-3, c='r', ls='--', alpha=0.3); axt.axhline(2.69 - 1e-3, c='r', ls='--', alpha=0.3)
 
 
-axr.set_xlabel(r'travel time $t$ [ms]', x=0.8)
+axr.set_xlabel(r'$2 V_{\mathrm{acc}}$ [V]', x=0.8)
 if tix: tick(axr, xmaj=0.1, ymaj=0.5)
 legend = axf.legend(loc='best')
