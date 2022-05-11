5e26014688d5852df7149ea046dbf587f0ddc45d
Mattmex00 <matteo-rossi@hotmail.it>
2022-05-11 18:54:55 +0200

    brutto gay

--- EsD4/ADC.py
+++ EsD4/ADC.py
@@ -6,7 +6,7 @@ from scipy import optimize
 def sin_func(t, A, nu,phi,B):
     return A*np.sin(2*np.pi*nu*t + phi) + B
 
-file_path = './data/sinusoide1v1Off.txt'
+file_path = "C:/Users/rossi/OneDrive/Documenti/GitHub/Lab3/EsD4/data/sinusoide1V.txt"
 with open(file_path,newline='') as csvfile:
 #convert csv removing comments and empty lines
 # fieldnames = ['Data']
@@ -16,8 +16,6 @@ with open(file_path,newline='') as csvfile:
 # data = list(csvreader);
     n=0;
     decimal_stream = np.array([elem for elem in csvreader]);
-    print(decimal_stream.dtype);
-    print(decimal_stream.shape[1]);
     bit_stream = np.zeros((decimal_stream.shape[1]-1)*8)
     for ind in range((decimal_stream.shape[1]-1)):
         a = np.binary_repr(int(decimal_stream[0,ind]),width=8);
@@ -26,6 +24,7 @@ with open(file_path,newline='') as csvfile:
             j=ind*8+i;
             bit_stream[j] = a[i];
 ind = np.arange(0,bit_stream.shape[0])
+plt.subplot(2,1,1)
 plt.scatter(ind,bit_stream)
 plt.xlim(200,12000)
 def moving_average(x, w):
@@ -38,4 +37,17 @@ bit_stream_mov1 = moving_average(bit_stream_mov,8)
 ind = np.arange(bit_stream_mov1.shape[0])
 plt.scatter(ind,bit_stream_mov1)
 plt.xlim(0.0,2000)
+popt,pcov=optimize.curve_fit(sin_func,ind,bit_stream_mov1,p0=np.array([1,0.002,1,1]))
+z=np.linspace(ind[0],ind[len(ind)-1],10000)
+plt.plot(z,sin_func(z,*popt))
+res=(bit_stream_mov1-sin_func(ind,*popt))/(3./255.)
+print(len(res))
+chisq=((res/1.)**2.).sum()
+print("chisq",chisq)
+plt.subplot(2,1,2)
+plt.errorbar(ind,res)
 plt.show()
+print("A = ", + popt[0])
+print("nu = ", + popt[1])
+print("phi = ", + popt[2])
+print("B = ", + popt[3])
